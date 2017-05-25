aecfeeb379d27398b9eeedb4f5e7692fa8ab760d
Scott <scott@monkeyhouse-media.com>
2017-05-24 22:08:39 -0700

    updated with new search feature URL and new homes

--- search_featured.php
+++ search_featured.php
@@ -33,9 +33,9 @@ intCount = parseInt(intCount) + 1;
 
 <body bgcolor="#b9babc" border="0" topmargin="0" onload="runSlideShow()">
    <div id="int-content-main"><?php include "lpt-header.php"; ?>
-            <div id="int-content"> 
+            <div id="int-content">
                 <a name="wolfnet_search" id="wolfnet_search"></a>
-                <iframe src="http://www.mlsfinder.com/wa_nwmls/gigharborluxuryhomes/index.cfm?action=newsearchsession&office_only=y" name="main" id="main" width="910" height="600" frameborder="0" onLoad="scrolltop()"></iframe>
+                <iframe src="http://search.gigharborhomes.com/search/results/list%3Fph%3D1300000%26zc%3D98332%2C98335%2C98333%2C98329%26pl%3D700000%26sort%3Dprice%5Fasc%26map%5Fbr%5Flng%3D%2D122%2E5594249%26map%5Ftl%5Flng%3D%2D122%2E679364%26map%5Ftl%5Flat%3D47%2E3874709%26map%5Fbr%5Flat%3D47%2E246964" name="main" id="main" width="910" height="600" frameborder="0" onLoad="scrolltop()"></iframe>
             </div>
 <?php include "lpt-footer.php"; ?>
     </div>
@@ -55,6 +55,3 @@ intCount = parseInt(intCount) + 1;
 
 </body>
 </html>
-
-
-
